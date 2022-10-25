061ec4290faec554c44cbc2166f45a52b5ace455
VIJAY SURIYA <69582968+svijaysuriya@users.noreply.github.com>
2022-10-25 12:28:49 +0530

    hello from browser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,26 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  before_action :require_user
-
-  helper_method :current_user, :logged_in?
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  def hello
+    render html: " hai"
   end
+  # protect_from_forgery with: :exception
 
-  def logged_in?
-    !!current_user
-  end
+  # before_action :require_user
 
-  def require_user
-    if logged_in?
-      current_user
-    else
-      flash[:alert] = "You must be logged in to perform this action"
-      redirect_to login_path
-    end
-  end
+  # helper_method :current_user, :logged_in?
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
+
+  # def logged_in?
+  #   !!current_user
+  # end
+
+  # def require_user
+  #   if logged_in?
+  #     current_user
+  #   else
+  #     flash[:alert] = "You must be logged in to perform this action"
+  #     redirect_to login_path
+  #   end
+  # end
 end
